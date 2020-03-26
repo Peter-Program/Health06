6e1f780b0a1b04454d377b28eec8a1ae87aa526c
Peter-Program <pterkderasmo@gmail.com>
2020-03-26 19:03:48 -0400

    Action Bar for Main/home screen and Settings template

--- app/src/main/java/com/example/health06/MainActivity.java
+++ app/src/main/java/com/example/health06/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.health06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // App bar menu methods start-------------------------------------------------------------------
+    // Menu item list is in res/menu/menu_main_activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);                                 // Initializing the app bar menu layout/style
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuMainActivitySettings:                                                     // When clicking on Settings
+                Toast.makeText(this, "Clicked Settings",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menuMainActivityAbout:
+                //Stuff
+                Toast.makeText(this, "Clicked About",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menuMainActivityCurrentSettings:
+                //Stuff
+                Toast.makeText(this, "Clicked Current Settings",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    // App bar menu methods end---------------------------------------------------------------------
 }
