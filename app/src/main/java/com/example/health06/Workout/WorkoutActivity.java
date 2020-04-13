324244a680b69022744909a934ed69d669faa9e3
kannanfire <adhithya.kannan@gmail.com>
2020-04-12 20:54:46 -0700

    adding workout files, adjusted strings.xml for workout tag

--- app/src/main/java/com/example/health06/Workout/WorkoutActivity.java
+++ app/src/main/java/com/example/health06/Workout/WorkoutActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.health06.BaseActivity;
 import com.example.health06.R;
 
@@ -16,7 +20,16 @@ public class WorkoutActivity extends BaseActivity {
         setContentView(R.layout.activity_workout);
 
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickEnterCustomWorkout(View view) {
+        Intent intent = new Intent(this, CustomWorkout.class);
+        startActivity(intent);
+    }
+
+    public void onClickEnterTodaysWorkout(View view) {
+        Intent intent = new Intent(this, TodaysWorkout.class);
+        startActivity(intent);
     }
 }
